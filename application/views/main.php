1e5aa1c1677158bb452131f2dced2d7476f6ee49
max_sa_max <max_sa_max@mail.ru>
2012-11-27 14:26:49 +0200

    add main view

---
+++ application/views/main.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>Main</title>
+	<link rel="stylesheet" type="text/css" href="application/src/css/bootstrap.css" />
+	<style type="text/css">
+		#centerer{
+			width:15%;
+			margin: 100px auto;
+		}
+
+		#main_wrapper{
+			width: 100%;
+		}
+	</style>
+
+	<script type="text/javascript" src="application/src/js/jquery.js"></script>
+	
+</head>
+<body>
+	<div id="main_wrapper">
+		<div id="centerer">
+			<div class="alert alert-info">
+			  	<h5>Страница в стадии разработки...</h5>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
