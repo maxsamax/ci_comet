06e8ce02cf92364b01beed33018b2b1a5c86c621
max_sa_max <max_sa_max@mail.ru>
2012-11-30 15:12:39 +0200

    Add crypt() for passwords

--- application/controllers/ajax.php
+++ application/controllers/ajax.php
@@ -6,7 +6,7 @@ class Ajax extends CI_Controller {
 	    $username = trim($_REQUEST['username']);
 	    $password = trim($_REQUEST['password']);
 
-		if ($this->redis->get($username) == $password) {
+		if ($this->redis->get($username) == crypt($password)) {
 			$this->load->view('main');
 		}else {
 			echo "$username's password incorrect. Try again!";
@@ -15,8 +15,9 @@ class Ajax extends CI_Controller {
 	public function reg_user() {
 		$username = trim($_REQUEST['username']);
 		$password = trim($_REQUEST['password']);
+		$password = crypt($password);
 		$this->redis->set($username, $password);
-		$this->load->view('main');
+		$this->load->view('login');
 	  }
 }
 
